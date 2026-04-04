3d91db86d46a54e0051e961f4c40b768e5dfcba9
JeetRana1 <104169225+JeetRana1@users.noreply.github.com>
2026-04-04 11:07:58 -0400

    refactor: standardize local API port to 3000 across frontend

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.__STREAMVERSE_CONFIG__ = {
   API_BASE: 'https://streamverse-api.ddns.net',
   META_API_BASE: 'https://streamverse-api.ddns.net/meta/tmdb',
-  LOCAL_API_BASE: 'http://127.0.0.1:3010/meta/tmdb',
-  LOCAL_META_API_BASE: 'http://127.0.0.1:3010/meta/tmdb',
+  LOCAL_API_BASE: 'http://127.0.0.1:3000/meta/tmdb',
+  LOCAL_META_API_BASE: 'http://127.0.0.1:3000/meta/tmdb',
   PROD_API_BASE: 'https://streamverse-api.ddns.net/meta/tmdb',
   PROD_META_API_BASE: 'https://streamverse-api.ddns.net/meta/tmdb',
   FALLBACK_API_BASE: 'https://consumet-api.vercel.app/meta/tmdb'
