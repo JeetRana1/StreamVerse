abaa3978d8bf83a6f37711c2ca3c040836bef801
JeetRana1 <104169225+JeetRana1@users.noreply.github.com>
2026-06-13 20:11:47 -0400

    Improve player streaming controls

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ window.__STREAMVERSE_CONFIG__ = {
   META_API_BASE: 'https://streamverse-api.ddns.net/meta/tmdb',
   LOCAL_API_BASE: 'http://127.0.0.1:3000/meta/tmdb',
   LOCAL_META_API_BASE: 'http://127.0.0.1:3000/meta/tmdb',
+  LOCAL_MEDIA_PROXY_BASE: 'http://127.0.0.1:3000',
   PROD_API_BASE: 'https://streamverse-api.ddns.net/meta/tmdb',
   PROD_META_API_BASE: 'https://streamverse-api.ddns.net/meta/tmdb',
   FALLBACK_API_BASE: 'https://consumet-api.vercel.app/meta/tmdb',
